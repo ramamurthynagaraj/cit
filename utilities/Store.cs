5176f03afe8d3e800e7218e6e465e23b1fac36c3
Nagaraj <ramamurthynagaraj@gmail.com>
2016-06-29 10:08:39 +0530

    Recording all initialized environments - Nagaraj

--- utilities/Store.cs
+++ utilities/Store.cs
@@ -1,4 +1,3 @@
-using System;
 using System.IO;
 using System.Linq;
 using Newtonsoft.Json.Linq;
@@ -19,7 +18,14 @@ namespace cit.utilities
 
         public static void Create(string envName)
         {
-            File.Create(GetFileNameFor(envName));
+            File.Create(GetFileNameFor(envName)).Dispose();
+            var defaultEnv = GetFileNameFor(Constants.DefaultEnvName);
+            var defaultEnvJson = GetJsonForFile(defaultEnv);
+            var newEnvironment = JObject.Parse($"{{'environment': ['{envName}']}}");
+            defaultEnvJson.Merge(newEnvironment, new JsonMergeSettings{
+                MergeArrayHandling = MergeArrayHandling.Union
+            });
+            File.WriteAllText(defaultEnv, defaultEnvJson.ToString());
         }
 
         public static void Clean(string envName)
@@ -30,8 +36,7 @@ namespace cit.utilities
         public static void Add(string envName, string keyName, string value)
         {
             var fileName = GetFileNameFor(envName);
-            var fileContent = File.ReadAllText(fileName);
-            var existingJson = JObject.Parse(string.IsNullOrEmpty(fileContent) ? @"{'cit': {}}": fileContent);
+            var existingJson = GetJsonForFile(fileName);
             var newItem = JObject.Parse($"{{'cit': {{'{keyName}': '{value}'}}}}");
             existingJson.Merge(newItem, new JsonMergeSettings{
                 MergeArrayHandling = MergeArrayHandling.Union
@@ -42,14 +47,19 @@ namespace cit.utilities
         public static void Remove(string envName, string keyName)
         {
             var fileName = GetFileNameFor(envName);
-            var fileContent = File.ReadAllText(fileName);
-            var existingJson = JObject.Parse(string.IsNullOrEmpty(fileContent) ? @"{'cit': {}}": fileContent);
+            var existingJson = GetJsonForFile(fileName);
             var allKeys = existingJson.GetValue("cit") as JObject;
             if(allKeys.Properties().Any(p => p.Name == keyName))
             {
                 allKeys.Property(keyName).Remove();
             }
             File.WriteAllText(fileName, existingJson.ToString());
+        }
+
+        private static JObject GetJsonForFile(string fileName)
+        {
+            var fileContent = File.ReadAllText(fileName);
+            return JObject.Parse(string.IsNullOrEmpty(fileContent) ? @"{'cit': {}}": fileContent);
         }
     }
 }
